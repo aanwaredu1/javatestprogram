bd04f557acee621d15f86e46831ebacdedbcaa45
AshrafAnwar <ashrafanwar@My1stLinuxVMOS.frontierlocal.net>
2018-07-21 12:30:19 -0400

    Initial

---
+++ If.java
@@ -0,0 +1,13 @@
+public class If{
+  public static void main (String args[]){
+    int num1 = 100;
+    int num2 = 150;
+    
+    if (num1 > num2){
+      // if command only run if statement is true otherwise use else command
+      System.out.println("num1 is bigger then num 2");
+    } else{
+      System.out.println("num1 is smaller then num2");
+    }
+  }
+}
